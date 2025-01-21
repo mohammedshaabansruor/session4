8331e2d86d491510cc91c266917f67f03d95e0d0
mohammedshaabansruor <shaabanmohammed490@gmail.com>
2025-01-21 21:59:20 +0200

    Q12

--- session 4c#/Program.cs
+++ session 4c#/Program.cs
@@ -269,12 +269,26 @@
             //}
             #endregion
             #region Q10
-            Console.WriteLine("please enter first string");
-            string st1 = Console.ReadLine();
-            Console.WriteLine("please enter  second string ");
-            string st2 = Console.ReadLine();
-            string FullSt = st1 + st2;
-            Console.WriteLine(FullSt);
+            //Console.WriteLine("please enter first string");
+            //string st1 = Console.ReadLine();
+            //Console.WriteLine("please enter  second string ");
+            //string st2 = Console.ReadLine();
+            //string FullSt = st1 + st2;
+            //Console.WriteLine(FullSt);
+            #endregion
+            #region Q12
+            Console.WriteLine("Please enter the size of array");
+            int size = int.Parse(Console.ReadLine()), sum = 0;
+            int[] Numbers = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"please ener array element no {i}");
+                Numbers[i] = int.Parse(Console.ReadLine());
+                sum += Numbers[i];
+
+            }
+            Console.WriteLine($"Sum of elements of array is {sum}");
             #endregion
         }
     }
