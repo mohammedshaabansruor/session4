5c6f6b5899e99036894d1564cfd6b6362fb1f232
mohammedshaabansruor <shaabanmohammed490@gmail.com>
2025-01-21 19:37:32 +0200

    Q6

--- session 4c#/Program.cs
+++ session 4c#/Program.cs
@@ -222,6 +222,17 @@
             }
 
             #endregion
+            #region Q6
+            Console.WriteLine("please enter a start");
+            int Start = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter a end ");
+            int End = int.Parse(Console.ReadLine());
+            for (int i = Start; i <= End; ++i)
+            {
+
+                Console.WriteLine($"{i}");
+            }
+            #endregion
         }
     }
 }
